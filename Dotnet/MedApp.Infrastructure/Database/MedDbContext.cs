24abb3018c315767ecf530f36518bef3b759b43e
gerald.aden <gerald@gmatech.com>
2023-07-26 19:11:14 -0700

    Removed commented out code.

--- Dotnet/MedApp.Infrastructure/Database/MedDbContext.cs
+++ Dotnet/MedApp.Infrastructure/Database/MedDbContext.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-// using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using MedApp.Infrastructure.Database.Entities;
 
 namespace MedApp.Infrastructure.Database;
@@ -22,10 +21,6 @@ public class MedDbContext : DbContext
     {
         modelBuilder.HasDefaultSchema("records");
 
-        // modelBuilder.ApplyConfiguration(new UserConfiguration());
-        // modelBuilder.ApplyConfiguration(new PatientConfiguration());
-        // modelBuilder.ApplyConfiguration(new AddressConfiguration());
-
         modelBuilder.Entity<Patient>()
             .ToTable("patients")
             .HasMany(p => p.Addresses)
@@ -43,65 +38,9 @@ public class MedDbContext : DbContext
         modelBuilder.Entity<Address>()
             .Property(p => p.CreatedAt)
                 .HasDefaultValueSql("NOW()");
-
-        // modelBuilder.Entity<Address>()
-        //     .ToTable("addresses");
-        // modelBuilder.Entity<Address>()
-        //     .Property(a => a.Street).IsRequired();
-        // modelBuilder.Entity<Address>()
-        //     .Property(a => a.City).IsRequired();
-        // modelBuilder.Entity<Address>()
-        //     .Property(a => a.State).IsRequired();
-        // modelBuilder.Entity<Address>()
-        //     .Property(a => a.ZipCode).IsRequired();
-
-        // modelBuilder.Entity<User>()
-        //     .ToTable("users");
     }
 
     public DbSet<User> Users { get; set; }
     public DbSet<Patient> Patients { get; set; }
     public DbSet<Address> Addresses { get; set; }
 }
-
-// public class UserConfiguration : IEntityTypeConfiguration<Address>
-// {
-//     public void Configure(EntityTypeBuilder<Address> builder)
-//     {
-//         builder.Property(u => u.CreatedAt)
-//             .HasColumnType("timestamp")
-//             .HasDefaultValueSql("GETUTCDATE()");
-
-//         builder.Property(u => u.UpdatedAt)
-//             .HasColumnType("timestamp");
-//     }
-// }
-
-// public class PatientConfiguration : IEntityTypeConfiguration<Patient>
-// {
-//     public void Configure(EntityTypeBuilder<Patient> builder)
-//     {
-//         builder.Property(p => p.DateOfBirth)
-//             .HasColumnType("date");
-
-//         builder.Property(p => p.CreatedAt)
-//             .HasColumnType("timestamp")
-//             .HasDefaultValueSql("NOW()");
-
-//         builder.Property(p => p.UpdatedAt)
-//             .HasColumnType("timestamp");
-//     }
-// }
-
-// public class AddressConfiguration : IEntityTypeConfiguration<User>
-// {
-//     public void Configure(EntityTypeBuilder<User> builder)
-//     {
-//         builder.Property(u => u.CreatedAt)
-//             .HasColumnType("timestamp")
-//             .HasDefaultValueSql("NOW()");
-
-//         builder.Property(u => u.UpdatedAt)
-//             .HasColumnType("timestamp");
-//     }
-// }
--- Dotnet/MedApp.Infrastructure/Repositories/IPatientRepository.cs
+++ Dotnet/MedApp.Infrastructure/Repositories/IPatientRepository.cs
@@ -6,7 +6,4 @@ public interface IPatientRepository
 {
     Task<IEnumerable<Patient>> GetPatientsAsync();
     Task AddPatientAsync(Patient patient);
-//     Task<Patient> GetPatientAsync(string id);
-//     Task<bool> UpdatePatientAsync(Patient patient);
-//     Task<bool> DeletePatientAsync(string id);
 }
